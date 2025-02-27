9affff9ebd648944c37925102245ddaf5cde7103
AgAnswers <aganswersai@gmail.com>
2025-02-27 18:41:47 +0000

    Allow self-signed cert on redis instance

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -5,7 +5,14 @@ console.log('REDIS_URL', process.env.REDIS_URL)
 // Create a Redis client
 export const redisClient = createClient({
   url: process.env.REDIS_URL!,
-  // password: process.env.REDIS_PASSWORD,
+  socket: {
+    reconnectStrategy: (retries) => {
+      console.log(`Redis reconnect attempt #${retries}`)
+      return Math.min(retries * 100, 3000) // Increasing backoff with a maximum of 3 seconds
+    },
+    tls: true,
+    rejectUnauthorized: false // Allow self-signed certificates
+  }
 })
 
 // Connect to the Redis server
